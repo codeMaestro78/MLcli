364df6c94bd4934b74b8252de500cb6b6f6cc270
LALANI DEVARSHI <thelogical369@gmail.com>
2026-01-18 01:15:19 +0530

    Changed version in __init__.py

--- mlcli/__init__.py
+++ mlcli/__init__.py
@@ -5,7 +5,7 @@ A modular, configuration-driven tool for training, evaluating, and tracking
 Machine Learning and Deep Learning models with both CLI and interactive TUI interfaces.
 """
 
-__version__ = "0.1.1"
+__version__ = "0.3.0"
 __author__ = "Devarshi Lalani"
 __licence__ = "MIT"
 
